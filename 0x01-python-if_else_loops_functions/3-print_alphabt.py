b8a7b031eaafd6611e003c94efc7a527a8d1e560
EkenoSamson <samsonekeno98@gmail.com>
2023-10-31 12:26:21 -0700

    updated task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@ number = 97
 
 while (number > 96 and number < 123):
     if (number != 101 and number != 113):
-        print(chr(number), end="")
+        print("{}".format(chr(number)), end="")
     number = number + 1
