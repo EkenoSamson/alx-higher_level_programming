7bf21880fd8439e257f6778eabc2f4c1048157d6
EkenoSamson <samsonekeno98@gmail.com>
2024-01-08 02:49:15 -0800

    super concept

---
+++ 0x0A-python-inheritance/Prac/test-3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# super concept in inheritance
+
+class Person:
+    def __init__(self, name, age):
+        self.age = age
+        self.name = name
+
+    def eatndrink(self):
+        print("Eat Biryani and drink Beer")
+    
+class Employee(Person):
+    def __init__(self, name, age, eno, esal):
+        self.name =  name
+        self.age = age
+        self.eno = eno
+        self.esal = esal
+
+    def work(self):
+        print("Codes Python projects")
+
+    def empInfo(self):
+        print(self.name)
+        print(self.age)
+        print(self.eno)
+        print(self.esal)
+
+e = Employee("Durga", 48, 872425, 10000)
+e.eatndrink()
+e.work()
+e.empInfo()
