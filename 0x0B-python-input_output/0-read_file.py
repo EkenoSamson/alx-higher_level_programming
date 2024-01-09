d253e4baeb4d69caa132831b22371566392b86d8
EkenoSamson <samsonekeno98@gmail.com>
2024-01-09 06:59:23 -0800

    fix coding style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# By Ekeno
+"""module for file reading"""
+
 
 def read_file(filename=""):
     """ Read a file in utf8 encoding and print to the stdout"""
-
     with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
