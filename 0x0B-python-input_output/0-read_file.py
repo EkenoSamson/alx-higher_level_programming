ee7a95939ca278eff2f23f5e8de10e41d1069da7
EkenoSamson <samsonekeno98@gmail.com>
2024-01-09 06:56:33 -0800

    add doc in read_file function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,8 @@
 # By Ekeno
 
 def read_file(filename=""):
+    """ Read a file in utf8 encoding and print to the stdout"""
+
     with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
         for line in lines:
