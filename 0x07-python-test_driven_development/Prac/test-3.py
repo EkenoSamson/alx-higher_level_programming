bf986c71bf6db8a11cc188bea4f634312a4caec2
EkenoSamson <samsonekeno98@gmail.com>
2024-01-12 01:28:03 -0800

    add a test in test-3

--- 0x07-python-test_driven_development/Prac/test-3.py
+++ 0x07-python-test_driven_development/Prac/test-3.py
@@ -5,7 +5,9 @@ class TesExample(unittest.TestCase):
     def test_greet_person(self):
         greeting_message = example.greet_person('Joe')
         self.assertEqual(greeting_message, "Welcome Joe")
-
+    
+    def test_greet_person_lowercase(self):
+        self.assertEqual(example.greet_person('joe'), "Welcome Joe")
 
 
 
