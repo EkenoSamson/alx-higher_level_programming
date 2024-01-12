cc82bfa7279d35d3a401f21f25b748b3c6ec40e7
EkenoSamson <samsonekeno98@gmail.com>
2024-01-12 01:22:43 -0800

    update test-3

--- 0x07-python-test_driven_development/Prac/test-3.py
+++ 0x07-python-test_driven_development/Prac/test-3.py
@@ -5,3 +5,9 @@ class TesExample(unittest.TestCase):
     def test_greet_person(self):
         greeting_message = example.greet_person('Joe')
         self.assertEqual(greeting_message, "Welcome Joe")
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
