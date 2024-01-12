a9a968c7eafc4e6eb65e7a71a9b856370888e5a9
EkenoSamson <samsonekeno98@gmail.com>
2024-01-12 02:19:23 -0800

    add subTest in test-3

--- 0x07-python-test_driven_development/Prac/test-3.py
+++ 0x07-python-test_driven_development/Prac/test-3.py
@@ -3,16 +3,19 @@ import unittest, example
 
 class TesExample(unittest.TestCase):
     def test_greet_person(self):
-        with self.subTest():
+        with self.subTest('Uppercase J'):
             greeting_message = example.greet_person('Joe')
             self.assertEqual(greeting_message, "Welcome Joe")
 
-        with self.subTest():
+        with self.subTest('Lowercase j'):
             self.assertEqual(example.greet_person('joe'), "Welcome Joe")
 
     def test_can_drink_alcohol(self):
         """ Test if he/she can drink alcohol """
-        self.assertTrue(example.can_drink_alcohol(21))
+        with self.subTest():
+            self.assertTrue(example.can_drink_alcohol(21))
+        with self.subTest():
+            self.assertFalse(example.can_drink_alcohol(16))
 
 
 
