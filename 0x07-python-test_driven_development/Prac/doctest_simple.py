c879d07a1a27dbdd880c08a76020d6fe6c335bd5
EkenoSamson <samsonekeno98@gmail.com>
2024-01-13 05:13:02 -0800

    add doctest example

---
+++ 0x07-python-test_driven_development/Prac/doctest_simple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# doctst_simple.py
+
+
+
+def my_function(a, b):
+    """
+    work with numbers:
+
+    >>> my_function(2, 3)
+    6
+
+    and strings:
+
+    >>> my_function('a', 3)
+    'aaa'
+    """
+    return (a * b)
